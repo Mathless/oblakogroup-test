1a8e61ed3eaeff3a55ab79cba0b5b574c2d6b2bf
tyoma <mrkalumb@gmail.com>
2022-08-22 13:24:14 +0300

    fix(second-step): heroku

--- src/config/database-base.config.ts
+++ src/config/database-base.config.ts
@@ -4,7 +4,7 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 export const baseConfig: ConnectionOptions = {
-  url: process.env.DATABASE_URL,
+  url: 'postgres://maibxgxcnextot:c9d7d8509e84626d017e0bc66c73e23f3c27a9d09793f0af679cda115d5d1e36@ec2-34-193-44-192.compute-1.amazonaws.com:5432/d57h7o3bq9c7ak',
   type: 'postgres',
   synchronize: false,
   migrationsRun: false,
