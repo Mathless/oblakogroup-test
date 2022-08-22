5317e460692953c7148353fe46dbd72f88eca8fb
tyoma <mrkalumb@gmail.com>
2022-08-22 11:54:25 +0300

    fix(second-step): heroku

--- src/config/database-base.config.ts
+++ src/config/database-base.config.ts
@@ -7,12 +7,8 @@ dotenv.config();
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 export const baseConfig: ConnectionOptions = {
+  url: process.env.DATABASE_URL,
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'postgres',
   synchronize: false,
   migrationsRun: false,
   namingStrategy: new SnakeNamingStrategy(),
