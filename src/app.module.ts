933cfd77649b69b88c50a70241eaaa45e35c2b3b
tyoma <mrkalumb@gmail.com>
2022-08-24 12:33:11 +0300

    fix(first-step): heroku graphql schema updating

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { join } from 'path';
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      introspection: true,
     }),
   ],
   controllers: [AppController],
